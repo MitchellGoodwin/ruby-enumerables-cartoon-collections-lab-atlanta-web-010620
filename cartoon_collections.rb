ea136a62f945be28b495e74ebdaf8b5f1e3ac8ce
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-12 18:44:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index |dwarf, index| do
+  array.each_with_index do |dwarf, index|
     print "#{index + 1}. #{dwarf}"
   end
 end
