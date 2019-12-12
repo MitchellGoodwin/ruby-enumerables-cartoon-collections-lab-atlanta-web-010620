74abaf82acfc079861712c00eea9939544450fec
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-12 18:57:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def long_planeteer_calls(array)
   # Your code here
-  result = array.select{|call| 
+  result = array.select{|call| call.length > 4
 end
 
 def find_the_cheese# code an argument here
