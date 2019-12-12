c150a65c58d78b58f128e100e563e6b65fc452ca
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-12 18:50:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(array)
   # Your code here
-  result = array.collect { |call| "#{call.capitalize}!"
+  result = array.collect { |call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
