91199197924d342323fc7e4272aca54647a43cab
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-12 18:43:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index |dwarf, index| do
-    print ""
+    print "#{index + 1}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
